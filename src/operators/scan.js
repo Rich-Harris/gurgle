0a41306ff84337042be5b31efeaf94b04b281b7e
Rich Harris <richard.a.harris@gmail.com>
2015-12-10 15:17:41 -0500

    allow value to be overridden in scan

--- src/operators/scan.js
+++ src/operators/scan.js
@@ -3,8 +3,10 @@ import stream from '../stream.js';
 export default function scan ( source, fn, acc ) {
 	const destination = stream();
 
+	destination.push( acc );
+
 	source.subscribe( value => {
-		acc = fn( acc, value );
+		acc = fn( destination.value, value );
 		destination.push( acc );
 	}, err => {
 		destination.error( err );
