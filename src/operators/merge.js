d79088c2388826df3b6f3802b042575de2718956
Rich-Harris <richard.a.harris@gmail.com>
2015-12-19 19:06:35 -0500

    only close merged stream once all inputs are closed

--- src/operators/merge.js
+++ src/operators/merge.js
@@ -1,16 +1,23 @@
 import stream from '../stream.js';
+import isClosed from '../helpers/isClosed.js';
 
 export default function merge ( ...sources ) {
 	const destination = stream();
 
+	function push ( value ) {
+		destination.push( value );
+	}
+
+	function error ( err ) {
+		destination.error( err );
+	}
+
+	function close () {
+		if ( sources.every( isClosed ) ) destination.close();
+	}
+
 	sources.forEach( source => {
-		source.subscribe( value => {
-			destination.push( value );
-		}, err => {
-			destination.error( err );
-		}, () => {
-			destination.close();
-		});
+		source.subscribe( push, error, close );
 	});
 
 	return destination;
