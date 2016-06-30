69db6852a2302395f8474a9a3ab44a1d9d3f17b6
Rich-Harris <richard.a.harris@gmail.com>
2016-06-30 19:38:21 -0400

    add interval option to fromGeolocation

--- src/sources/fromGeolocation.js
+++ src/sources/fromGeolocation.js
@@ -1,11 +1,33 @@
 import stream from '../stream.js';
 
-export default function fromGeolocation ( options ) {
+export default function fromGeolocation ( options = {} ) {
+	const interval = options.interval;
+	let closed = false;
+	let watchId;
+
 	const source = stream( () => {
-		navigator.geolocation.clearWatch( id );
+		if ( interval ) {
+			closed = true;
+		} else {
+			navigator.geolocation.clearWatch( watchId );
+		}
 	});
 
-	const id = navigator.geolocation.watchPosition( source.push, source.error, options );
+	if ( interval ) {
+		const success = position => {
+			if ( closed ) return;
+
+			stream.push( position );
+			setTimeout( check, interval );
+		};
+
+		const check = () => {
+			if ( closed ) return;
+			navigator.geolocation.getCurrentPosition( success, source.error, options );
+		};
+	} else {
+		watchId = navigator.geolocation.watchPosition( source.push, source.error, options );
+	}
 
 	return source;
 }
