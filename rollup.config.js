5c1883e9abf2b0e345715283c15318ae97139452
Rich Harris <richard.a.harris@gmail.com>
2016-07-05 18:15:06 -0400

    fix build

--- rollup.config.js
+++ rollup.config.js
@@ -2,5 +2,10 @@ import buble from 'rollup-plugin-buble';
 
 export default {
 	entry: 'src/gurgle.js',
-	plugins: [ buble() ]
+	plugins: [ buble() ],
+	moduleName: 'gurgle',
+	targets: [
+		{ format: 'es', dest: 'dist/gurgle.es.js' },
+		{ format: 'umd', dest: 'dist/gurgle.umd.js' }
+	]
 };
