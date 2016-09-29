c0a6e94f64372a7f483f77ae48850dd5d9d78d80
Rich-Harris <richard.a.harris@gmail.com>
2016-09-28 23:06:48 -0400

    flatMap operator closes child streams when source stream closes

--- src/operators/flatMap.js
+++ src/operators/flatMap.js
@@ -3,13 +3,34 @@ import stream from '../stream.js';
 export default function flatMap ( source, streamGenerator ) {
 	const destination = stream();
 
+	let childSources = [];
+	let closed = false;
+
 	source.subscribe(
 		value => {
-			const next = streamGenerator( value );
-			next.subscribe( value => destination.push( value ) );
+			const child = streamGenerator( value );
+			childSources.push( child );
+
+			child.subscribe(
+				value => {
+					if ( !closed ) destination.push( value );
+				},
+				destination.error,
+				() => {
+					const index = childSources.indexOf( child );
+					if ( ~index ) childSources.splice( index, 1 );
+				}
+			);
 		},
 		destination.error,
-		destination.close // TODO should this stay open until all generated streams have closed? presumably
+		() => {
+			closed = true;
+
+			childSources.forEach( child => child.close() );
+			childSources = [];
+
+			destination.close();
+		}
 	);
 
 	return destination;
